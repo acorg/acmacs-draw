8c86d1cf641f56885d9d6fd71bbe0bf4443d566a
Eugene Skepner <github@skepner.eu>
2017-01-25 17:50:59 +0100

    new surface system development

--- cc/surface.hh
+++ cc/surface.hh
@@ -9,24 +9,27 @@
 
 // ----------------------------------------------------------------------
 
-template <char Tag> class SizeScale
+namespace _acmacs_draw_internal
 {
- public:
-    inline explicit SizeScale(double aValue) : mValue(aValue) {}
-    inline SizeScale(const SizeScale& a) = default;
-    inline SizeScale& operator = (double aValue) { mValue = aValue; return *this; }
-    inline double value() const { return mValue; }
-    inline SizeScale operator / (double a) const { return SizeScale{mValue / a}; }
-    inline SizeScale operator * (double a) const { return SizeScale{mValue * a}; }
-    inline SizeScale& operator *= (double a) { mValue *= a; return *this; }
-    inline SizeScale operator - () const { return SizeScale{- mValue}; }
+    template <char Tag> class SizeScale
+    {
+     public:
+        inline explicit SizeScale(double aValue) : mValue(aValue) {}
+        inline SizeScale(const SizeScale& a) = default;
+        inline SizeScale& operator = (double aValue) { mValue = aValue; return *this; }
+        inline double value() const { return mValue; }
+        inline SizeScale operator / (double a) const { return SizeScale{mValue / a}; }
+        inline SizeScale operator * (double a) const { return SizeScale{mValue * a}; }
+        inline SizeScale& operator *= (double a) { mValue *= a; return *this; }
+        inline SizeScale operator - () const { return SizeScale{- mValue}; }
 
- private:
-    double mValue;
-};
+     private:
+        double mValue;
+    };
+}
 
-using Pixels = SizeScale<'P'>; // size in pixels, indepenent from the surface internal coordinate system
-using Scaled = SizeScale<'S'>; // size in the surface internal coordinate system
+using Pixels = _acmacs_draw_internal::SizeScale<'P'>; // size in pixels, indepenent from the surface internal coordinate system
+using Scaled = _acmacs_draw_internal::SizeScale<'S'>; // size in the surface internal coordinate system
 
 // ----------------------------------------------------------------------
 
