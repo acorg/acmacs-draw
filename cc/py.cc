d77b4fe3b9b620f37b5c063cad794c906ba75311
Eugene Skepner <github@skepner.eu>
2017-06-07 09:38:25 +0200

    acmacs_draw_backend python module

--- cc/py.cc
+++ cc/py.cc
@@ -22,6 +22,9 @@ PYBIND11_MODULE(acmacs_draw_backend, m)
     m.def("distinct_colors", &Color::distinct_colors);
 
     py::class_<Surface>(m, "Surface")
+            .def("new_page", &Surface::new_page)
+            .def("line_p", [](Surface& aSurface, double x1, double y1, double x2, double y2, std::string color, double width) { aSurface.line({x1, y1}, {x2, y2}, color, Pixels{width}); }, py::arg("x1"), py::arg("y1"), py::arg("x2"), py::arg("y2"), py::arg("color"), py::arg("width"))
+            .def("line_s", [](Surface& aSurface, double x1, double y1, double x2, double y2, std::string color, double width) { aSurface.line({x1, y1}, {x2, y2}, color, Scaled{width}); }, py::arg("x1"), py::arg("y1"), py::arg("x2"), py::arg("y2"), py::arg("color"), py::arg("width"))
             ;
 
     py::class_<SurfaceCairo, Surface>(m, "SurfaceCairo")
