781b94ddd505fc7811974b78e62a85faffbd40e5
Eugene Skepner <github@skepner.eu>
2018-05-02 17:39:51 +0200

    toolkit.js development

--- js/utils.js
+++ js/utils.js
@@ -39,6 +39,38 @@ export function offset_sub(offset1, offset2) {
 
 // ----------------------------------------------------------------------
 
+// https://stackoverflow.com/questions/4810841/how-can-i-pretty-print-json-using-javascript
+export function json_syntax_highlight(data) {
+    data = data.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return data.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, match => {
+        var cls = 'json-syntax-number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'json-syntax-key';
+            }
+            else if (/^"[0-9a-f]{24}"/.test(match)) {
+                cls = 'json-syntax-object-id';
+            }
+            else {
+                cls = 'json-syntax-string';
+            }
+        }
+        else if (/true|false/.test(match)) {
+            cls = 'json-syntax-boolean';
+        }
+        else if (/null/.test(match)) {
+            cls = 'json-syntax-null';
+        }
+        if (cls === 'json-syntax-object-id') {
+            const id_itself = match.replace(/"/g, '');
+            const href = "/doc/" + id_itself;
+            return `"<a class="json-syntax-object-id" href="${href}" target="_blank">${id_itself}</a>"`;
+        }
+        else
+            return '<span class="' + cls + '">' + match + '</span>';
+    });
+}
+
 // ----------------------------------------------------------------------
 /// Local Variables:
 /// eval: (if (fboundp 'eu-rename-buffer) (eu-rename-buffer))
