d65c9a43b5e33108ad5b541ec62620a6bfb70d38
Eugene Skepner <github@skepner.eu>
2020-01-30 11:05:00 +0100

    world map size adjusted (still incorrect, seems to be path is not quite correct)

--- cc/continent-path.hh
+++ cc/continent-path.hh
@@ -7,7 +7,8 @@
 
 namespace continent_path
 {
-    constexpr const double continent_map_size[2] = {686.475, 346.750};
+    // constexpr const double continent_map_size[2] = {686.475, 346.750};
+    constexpr const double continent_map_size[2] = {660.0, 320.0};
 
     std::pair<const double*, const double*> continent_map_path(std::string continent);
 }
